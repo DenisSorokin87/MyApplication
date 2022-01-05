c68fc655d126c287f1d936679799f02f09e1a5dc
Denis <Denis.sor87@gmail.com>
2022-01-05 15:17:40 +0200

    Contact Read (With bugs)

--- app/src/main/java/com/denis/myapplication/ContactsFavorites.kt
+++ app/src/main/java/com/denis/myapplication/ContactsFavorites.kt
@@ -26,28 +26,15 @@ class ContactsFavorites : Fragment(R.layout.fragment_contacts) {
             ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_CONTACTS)
 
         if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            println("Permission was granted before")
             readContacts()
         } else {
+            println("The permission was noe granted before, opening permission dialog")
             ActivityCompat.requestPermissions(
                 requireActivity(), arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE_PERMISSION_READ_CONTACTS)
         }
-
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-
-        when(requestCode){
-            REQUEST_CODE_PERMISSION_READ_CONTACTS ->  if (grantResults.isNotEmpty()
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                readContacts();
-            }else println("Permission Denied")
-
-        }
-    }
 
     private fun readContacts() {
         val contactsList: ListView? = view?.findViewById(R.id.contacts_list)
@@ -61,16 +48,10 @@ class ContactsFavorites : Fragment(R.layout.fragment_contacts) {
         requireActivity().startManagingCursor(cursor)
 
         val contacstToList = intArrayOf(android.R.id.text1, android.R.id.text2)
-        var simple: SimpleCursorAdapter = SimpleCursorAdapter(requireActivity(),android.R.layout.simple_list_item_2, cursor, contactsFromPhone, contacstToList )
+        val simple: SimpleCursorAdapter = SimpleCursorAdapter(requireActivity(),android.R.layout.simple_list_item_2, cursor, contactsFromPhone, contacstToList )
         if (contactsList != null) {
             contactsList.adapter  = simple
         }
-//
-//        val contactsList: ArrayAdapter<String> = ArrayAdapter<String>(
-//            context,
-//            R.layout.users,
-//            cursor
-//        )
 
     }
 }
