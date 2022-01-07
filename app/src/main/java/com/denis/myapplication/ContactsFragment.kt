356cbe219674bc8733a32c3319a36042cd8742e9
Denis <Denis.sor87@gmail.com>
2022-01-07 12:01:18 +0200

    Permission Register try

--- app/src/main/java/com/denis/myapplication/ContactsFragment.kt
+++ app/src/main/java/com/denis/myapplication/ContactsFragment.kt
@@ -1,65 +1,93 @@
 package com.denis.myapplication
 
 import android.Manifest
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.database.CursorWrapper
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.view.View
-import android.widget.ListView
-import android.widget.SimpleCursorAdapter
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.base.CharMatcher
+import pub.devrel.easypermissions.EasyPermissions
 
 
 class ContactsFragment : Fragment(R.layout.fragment_contacts) {
 
-    val REQUEST_CODE_PERMISSION_READ_CONTACTS: Int = 1
+    private val READ_STORAGE_PERMKISSION_REQUEST = 123
+    private lateinit var image: ImageView
     private lateinit var contactsList: ListView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         contactsList = view.findViewById(R.id.contacts_list)
+//        addListenerOnListView()
+        image = view.findViewById(R.id.image)
+        val button: Button = view.findViewById(R.id.pickImageBtn)
+        button.setOnClickListener {
+            getPhotoPermissionResultLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        val getContactsBtn: Button = view.findViewById(R.id.getContacts)
+        getContactsBtn.setOnClickListener{
+            getContactsPermissionResultLauncher.launch(Manifest.permission.READ_CONTACTS)
+        }
+    }
 
-        val permissionStatus =
-            ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.READ_CONTACTS)
+    private fun openPhotoPicker() {
 
-        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            resultLauncher.launch(intent)
+
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if (result.resultCode == Activity.RESULT_OK && result.data != null){
+            val chosenPhoto: Uri? = result.data!!.data
+            Glide.with(this).load(chosenPhoto).into(image)
+        }
+    }
+
+    private val getPhotoPermissionResultLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+        if(isGranted){
+            openPhotoPicker()
+        }else{
+            println("Permission Denied")
+        }
+    }
+
+    private val getContactsPermissionResultLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+        if(isGranted){
             readContacts()
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(), arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE_PERMISSION_READ_CONTACTS)
-        }
-        addListenerOnListView()
-    }
-
-    private fun addListenerOnListView() {
-//        contactsList.onItemClickListener = OnItemClickListener { parent, view, position, id ->
-//           var contact =parent.getItemAtPosition(position)
-//            val intent = Intent(Intent.ACTION_DIAL)
-//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//            Uri.parse("tel" + CharMatcher.digit().retainFrom(contact.toString()))
-//            requireActivity().startActivity(intent)
-//        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-
-        when(requestCode){
-            REQUEST_CODE_PERMISSION_READ_CONTACTS ->  if (grantResults.isNotEmpty()
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                readContacts();
-            }else println("Permission Denied")
-
+        }else{
+            println("Permission Denied")
         }
     }
+
+//    private fun addListenerOnListView() {
+//        contactsList.onItemClickListener =
+//            AdapterView.OnItemClickListener { parent, view, position, id ->
+//                var contact = parent.getItemAtPosition(position)
+//                println((contact as ContentResolver))
+//
+//                val intent = Intent(Intent.ACTION_DIAL)
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+////                Uri.parse("tel" + CharMatcher.digit().retainFrom(contact.toString()))
+//                requireActivity().startActivity(intent)
+//            }
+//    }
 
     private fun readContacts() {
 
--- app/src/main/java/com/denis/myapplication/MainActivity.java
+++ app/src/main/java/com/denis/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         }
         bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
 
 
